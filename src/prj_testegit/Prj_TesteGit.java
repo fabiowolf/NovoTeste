90deb7fb4cc70d06d5bddc050ac2245c50e11b69
DEV_MACHINE <DEV_MACHINE@10.0.2.15>
2013-06-23 19:17:45 -0300

    Master

---
+++ src/prj_testegit/Prj_TesteGit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prj_testegit;
+
+/**
+ *
+ * @author DEV_MACHINE
+ */
+public class Prj_TesteGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Repositorio Git! Teste");
+    }
+}
